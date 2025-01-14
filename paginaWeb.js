925c41501b96a727340589830026c40c543f83fb
elizaRoF <floresdayana2rod@gmail.com>
2025-01-14 05:45:38 -0500

    Delete paginaWeb.js

--- paginaWeb.js
+++
@@ -1,8 +0,0 @@
-const secciones = document.querySelectorAll('.seccion');
-const informacionOculta = document.querySelectorAll('.informacion-oculta');
-
-secciones.forEach((seccion, index) => {
-  seccion.addEventListener('click', () => {
-    informacionOculta[index].classList.toggle('mostrar');
-  });
-});
